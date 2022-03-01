92dd0ec4ba2af69d00ced22691cdabf0c92308fa
rib-president <fnslfnslfnslfnslfnsl@gmail.com>
2022-03-01 11:44:43 +0900

    rooney 2022-03-01 11:43 first upload project

---
+++ FreeNext/src/main/java/com/teamb/freenext/FreeNextApplication.java
@@ -0,0 +1,13 @@
+package com.teamb.freenext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeNextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeNextApplication.class, args);
+	}
+
+}
